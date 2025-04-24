7417bb4256fae150b1248a22b67969897272cc2e
Korede Faleye <akfaleye@gmail.com>
2025-04-24 03:03:07 +0100

    Socket IO chat test

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("Hello, Korede!")
+// 1. Packages
+import express from "express"
+import http from "http"
+
+import { fileURLToPath } from "node:url"
+import { dirname, join } from "node:path"
+import { Server } from "socket.io"
+
+// 2. Instances
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+
+// 3. Serving HTML File
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(__dirname, "index.html")
+app.get("/", (req, res) => res.sendFile(join(__dirname, "index.html")))
+
+
+// 4. Define a connection event handler
+io.on("connection", (client) => {
+  console.log("User connected to Server ✔")
+
+  client.emit("message", "Welcome dude!")
+  
+  client.on("disconnect", () => {
+    console.log("User disconnected from Server ✖")
+  })
+
+  client.on("message", (message) => {
+    console.log("Message: ", message)
+    client.broadcast.emit("message", message)
+  })
+})
+
+
+
+// 5. Start the server
+const PORT = 3000
+server.listen(PORT, () => console.log(`Sever running on port: ${PORT}`))
+
+
+
